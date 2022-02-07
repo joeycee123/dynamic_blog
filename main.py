f1785d755f9469e2f2286d5471567381c8699c9c
Joe <joeycee123@hotmail.com>
2022-02-07 19:19:54 +1300

    Add gunicorn

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ from flask_gravatar import Gravatar
 # below is how you bring form from another .py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from functools import wraps
+import os
 
 
 
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100,
@@ -28,7 +29,7 @@ gravatar = Gravatar(app, size=100,
                     use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
